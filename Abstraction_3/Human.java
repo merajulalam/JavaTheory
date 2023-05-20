a97514b42ab4c041935ad6e1dc2c7904b2ad3501
Md. Merajul Alam Meraj <122860838+meraj5632@users.noreply.github.com>
2023-05-21 03:25:17 +0600

    Add files via upload

---
+++ Abstraction_3/Human.java
@@ -0,0 +1,19 @@
+package Abstraction_3;
+
+public class Human extends Animal
+{
+    public void eat()
+    {
+        System.out.println("Human is eating Burger");
+    }
+
+    public void move()
+    {
+        System.out.println("Human moves slowly");
+    }
+
+    public void talk()
+    {
+        System.out.println("Human can talk");
+    }
+}
